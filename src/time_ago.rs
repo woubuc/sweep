bba76f564242d57f8229b25285f7cb2515d826b9
Wouter <wou.buc@gmail.com>
2018-04-13 00:14:35 +0200

    Use floats for time display instead of converting all the time

--- src/time_ago.rs
+++ src/time_ago.rs
@@ -1,17 +1,17 @@
 pub fn time_ago(seconds : u64) -> String {
-    let mut seconds = seconds;
+    let mut seconds = seconds as f64;
 
-    let days : u64 = (seconds as f64 / 86400.0).floor() as u64;
-    if days > 31 { return "More than a month ago".to_owned(); }
-    else if days >= 1 { return format!("{}d ago", days); }
+    let days = (seconds / 86400.0).floor();
+    if days > 31.0 { return "More than a month ago".to_owned(); }
+    if days >= 1.0 { return format!("{}d ago", days); }
     
-    seconds -= days * 86400;
-    let hours : u64 = (seconds as f64 / 3600.0).floor() as u64;
+    seconds -= days * 86400.0;
+    let hours = (seconds / 3600.0).floor();
 
-    seconds -= hours * 3600;
-    let minutes : u64 = (seconds as f64 / 60.0).floor() as u64;
-    if hours > 1 { return format!("{}h {}m ago", hours, minutes); }
+    seconds -= hours * 3600.0;
+    let minutes = (seconds / 60.0).floor();
+    if hours > 1.0 { return format!("{}h {}m ago", hours, minutes); }
     
-    seconds -= minutes * 60;
+    seconds -= minutes * 60.0;
     return format!("{}m {}s ago", minutes, seconds);
 }
