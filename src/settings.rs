dfbc4098fa829e1cb4abd9354c53421fb144f1b2
Wouter <wou.buc@gmail.com>
2018-04-15 13:38:27 +0200

    --help text formatting

--- src/settings.rs
+++ src/settings.rs
@@ -8,17 +8,17 @@ This utility cleans up build & dependency directories in old projects to
 preserve disk space
 
 USAGE:
-	node-cleanup [FLAGS] <PATH>...
+    node-cleanup [FLAGS] <PATH>...
 
 ARGS:
-	PATH   One or more directories where the utility should start searching
-		   If omitted, will use the current working directory
+    <PATH>...   One or more directories where the utility should start searching
+                If omitted, will use the current working directory
 
 FLAGS:
-	-a, --all     Remove directories even in recently used projects
-	-f, --force   Skip confirmation before removing directories
+    -a, --all     Remove directories even in recently used projects
+    -f, --force   Skip confirmation before removing directories
 
-	-h, --help    Shows this help
+    -h, --help    Shows this help
 
 More info: github.com/woubuc/node-cleanup
 Questions, bugs & other issues: github.com/woubuc/node-cleanup/issues";
